fb5a05f207d3066f87f9d95afeecda453fc8f1e2
rubenszinho <rubenszinho@users.noreply.github.com>
2023-04-25 04:50:23 +0000

    deploy: c0510a1846b2501c5ae6d484306643eb2fd45cca

--- sw.js
+++ sw.js
@@ -1,25 +1,24 @@
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('game-store-cache').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/static/js/bundle.js',
-          '/static/js/main.chunk.js',
-          '/static/js/vendors~main.chunk.js',
-          '/manifest.json',
-          '/logo192.png',
-          '/logo512.png',
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  event.waitUntil(
+    caches.open('game-store-cache').then((cache) => {
+      return cache.addAll([
+        '/gamestore/',
+        '/gamestore/index.html',
+        '/gamestore/static/js/bundle.js',
+        '/gamestore/static/js/main.chunk.js',
+        '/gamestore/static/js/vendors~main.chunk.js',
+        '/gamestore/manifest.json',
+        '/gamestore/logo192.png',
+        '/gamestore/logo512.png',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
